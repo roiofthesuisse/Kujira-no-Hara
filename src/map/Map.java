03c7f1041c53daca2d8afabb595b14325e719d67
RoiOfTheSuisse <pyer.blobfish@live.fr>
2015-12-24 18:45:47 +0100

    test lolilol

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,11 +1,8 @@
 package map;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Constructor;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
