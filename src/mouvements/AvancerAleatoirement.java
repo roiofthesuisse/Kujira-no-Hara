b6a5e0f69183f396c772e8c6baf236afaebbcd75
RoiOfTheSuisse <pyer.blobfish@live.fr>
2016-08-21 23:53:20 +0200

    Ajout de la Map 3

--- src/mouvements/AvancerAleatoirement.java
+++ src/mouvements/AvancerAleatoirement.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 import main.Fenetre;
 import map.Event.Direction;
-import utilitaire.GenerateurAleatoire;
 import utilitaire.Maths;
 
 /**
@@ -13,13 +12,12 @@ import utilitaire.Maths;
 public class AvancerAleatoirement extends Avancer {
 	//constantes
 	public static final int NOMBRE_DE_DIRECTIONS_POSSIBLES = 4; 
-	private static GenerateurAleatoire rand = new GenerateurAleatoire();
 	
 	/** 
 	 * Constructeur explicite 
 	 */
 	public AvancerAleatoirement() {
-		super(rand.nextInt(NOMBRE_DE_DIRECTIONS_POSSIBLES), Fenetre.TAILLE_D_UN_CARREAU);
+		super(Maths.generateurAleatoire.nextInt(NOMBRE_DE_DIRECTIONS_POSSIBLES), Fenetre.TAILLE_D_UN_CARREAU);
 	}
 	
 	/**
@@ -32,14 +30,14 @@ public class AvancerAleatoirement extends Avancer {
 	
 	@Override
 	public final void reinitialiserSpecifique() {
-		int nouvelleDirection = rand.nextInt(NOMBRE_DE_DIRECTIONS_POSSIBLES);
+		int nouvelleDirection = Maths.generateurAleatoire.nextInt(NOMBRE_DE_DIRECTIONS_POSSIBLES);
 		//ne pas faire demi-tour, ça donne l'impression que l'Event ne sait pas où il va
 		if ( (  this.direction==Direction.BAS && nouvelleDirection==Direction.HAUT) 
 			|| (this.direction==Direction.GAUCHE && nouvelleDirection==Direction.DROITE) 
 			|| (this.direction==Direction.DROITE && nouvelleDirection==Direction.GAUCHE) 
 			|| (this.direction==Direction.HAUT && nouvelleDirection==Direction.BAS) 
 		) {
-			nouvelleDirection += (1 + rand.nextInt(NOMBRE_DE_DIRECTIONS_POSSIBLES-1));
+			nouvelleDirection += (1 + Maths.generateurAleatoire.nextInt(NOMBRE_DE_DIRECTIONS_POSSIBLES-1));
 			nouvelleDirection = Maths.modulo(nouvelleDirection, NOMBRE_DE_DIRECTIONS_POSSIBLES);
 		}
 		this.direction = nouvelleDirection;
