f05a4a73c52f641155517c7a44054ec76f12117f
RoiOfTheSuisse <pyer.blobfish@live.fr>
2016-10-26 20:30:24 +0200

    autotile8

--- src/map/Autotile.java
+++ src/map/Autotile.java
@@ -23,6 +23,26 @@ public class Autotile {
 	public static final int HAUTEUR_AUTOTILE = 4*Fenetre.TAILLE_D_UN_CARREAU;
 	public static final int TAILLE_MORCEAU = Fenetre.TAILLE_D_UN_CARREAU/2;
 	
+	//quart bas gauche
+	public static final int X_COIN_SORTANT_BAS_GAUCHE = 0;
+	public static final int Y_COIN_SORTANT_BAS_GAUCHE = 7;
+	
+	//quart haut gauche
+	public static final int X_PLEIN_HAUT_GAUCHE = 2;
+	public static final int Y_PLEIN_HAUT_GAUCHE = 4;
+	public static final int X_COIN_RENTRANT_HAUT_GAUCHE = 4;
+	public static final int Y_COIN_RENTRANT_HAUT_GAUCHE = 0;
+	public static final int X_BORD_VERTICAL_HAUT_GAUCHE = 0;
+	public static final int Y_BORD_VERTICAL_HAUT_GAUCHE = 4;
+	public static final int X_BORD_HORIZONTAL_HAUT_GAUCHE = 2;
+	public static final int Y_BORD_HORIZONTAL_HAUT_GAUCHE = 2;
+	public static final int X_COIN_SORTANT_HAUT_GAUCHE = 0;
+	public static final int Y_COIN_SORTANT_HAUT_GAUCHE = 2;
+	
+	//quart haut droite
+	public static final int X_COIN_SORTANT_HAUT_DROITE = 5;
+	public static final int Y_COIN_SORTANT_HAUT_DROITE = 2;
+	
 	private String nomImage;
 	private BufferedImage image;
 	public boolean passabilite;
@@ -63,15 +83,15 @@ public class Autotile {
 	
 	/**
 	 * Calculer l'apparence du carreau liable en fonction de son voisinnage.
-	 * @param xEcran coordonnee x de la case en nombre de carreaux
-	 * @param yEcran coordonnee y de la case en nombre de carreaux
-	 * @param largeurMap largeur de la Map en nombre de carreaux
-	 * @param hauteurMap hauteur de la Map en nombre de carreaux
+	 * @param x coordonnee x de la case sur la Map (en nombre de carreaux)
+	 * @param y coordonnee y de la case sur la Map (en nombre de carreaux)
+	 * @param largeurMap largeur de la Map (en nombre de carreaux)
+	 * @param hauteurMap hauteur de la Map (en nombre de carreaux)
 	 * @param numeroCarreau numéro de ce carreau de décor issu du Tileset
 	 * @param layer une des trois couches de décor de l'éditeur de Maps
 	 * @return carreau liable avec la bonne apparence
 	 */
-	public BufferedImage calculerAutotile(final int xEcran, final int yEcran, final int largeurMap, final int hauteurMap, final int numeroCarreau, final int[][] layer) {
+	public BufferedImage calculerAutotile(final int x, final int y, final int largeurMap, final int hauteurMap, final int numeroCarreau, final int[][] layer) {
 		// On veut déterminer les connexions du carreau
 		boolean connexionBas = false;
 		boolean connexionGauche = false;
@@ -83,23 +103,23 @@ public class Autotile {
 		boolean connexionHautDroite = false;
 		
 		// On considère que le bord de l'écran est liable lui aussi
-		if (yEcran == 0) {
+		if (y == 0) {
 			//bord supérieur de l'écran
 			connexionHaut = true;
 			connexionHautGauche = true;
 			connexionHautDroite = true;
-		} else if (yEcran == largeurMap-1) {
+		} else if (y == hauteurMap-1) {
 			//bord inférieur de l'écran
 			connexionBas = true;
 			connexionBasGauche = true;
 			connexionBasDroite = true;
 		}
-		if (xEcran == 0) {
+		if (x == 0) {
 			//bord gauche de l'écran
 			connexionGauche = true;
 			connexionHautGauche = true;
 			connexionBasGauche = true;
-		} else if (xEcran == hauteurMap-1) {
+		} else if (x == largeurMap-1) {
 			//bord droit de l'écran
 			connexionDroite = true;
 			connexionHautDroite = true;
@@ -109,41 +129,41 @@ public class Autotile {
 		// On regarde les connexions possibles avec les carreaux voisins
 		int numeroVoisin;
 		if (!connexionHaut) {
-			numeroVoisin = layer[xEcran][yEcran-1];
-			connexionHaut = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x][y-1];
+			connexionHaut = (numeroVoisin == numeroCarreau) || this.cousins.contains(numeroVoisin);
 		}
 		if (!connexionBas) {
-			numeroVoisin = layer[xEcran][yEcran+1];
-			connexionBas = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x][y+1];
+			connexionBas = (numeroVoisin == numeroCarreau) || this.cousins.contains(numeroVoisin);
 		}
 		if (!connexionGauche) {
-			numeroVoisin = layer[xEcran-1][yEcran];
-			connexionGauche = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x-1][y];
+			connexionGauche = (numeroVoisin == numeroCarreau) || this.cousins.contains(numeroVoisin);
 		}
 		if (!connexionDroite) {
-			numeroVoisin = layer[xEcran+1][yEcran];
-			connexionDroite = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x+1][y];
+			connexionDroite = (numeroVoisin == numeroCarreau) || this.cousins.contains(numeroVoisin);
 		}
 		
 		// Selon les cas, ceux-là ne sont pas forcément utiles pour dessiner le carreau
 		if (!connexionHautGauche) {
-			numeroVoisin = layer[xEcran-1][yEcran-1];
-			connexionHaut = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x-1][y-1];
+			connexionHautGauche = (numeroVoisin == numeroCarreau) || this.cousins.contains(numeroVoisin);
 		}
 		if (!connexionHautDroite) {
-			numeroVoisin = layer[xEcran+1][yEcran-1];
-			connexionDroite = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x+1][y-1];
+			connexionHautDroite = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
 		}
 		if (!connexionBasGauche) {
-			numeroVoisin = layer[xEcran-1][yEcran+1];
-			connexionGauche = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x-1][y+1];
+			connexionBasGauche = (numeroVoisin == numeroCarreau) || this.cousins.contains(numeroVoisin);
 		}
 		if (!connexionBasDroite) {
-			numeroVoisin = layer[xEcran+1][yEcran+1];
-			connexionBas = numeroVoisin == numeroCarreau || this.cousins.contains(numeroVoisin);
+			numeroVoisin = layer[x+1][y+1];
+			connexionBasDroite = (numeroVoisin == numeroCarreau) || this.cousins.contains(numeroVoisin);
 		}
 		
-		BufferedImage resultat = new BufferedImage(Fenetre.TAILLE_D_UN_CARREAU, Fenetre.TAILLE_D_UN_CARREAU, Lecteur.TYPE_DES_IMAGES);
+		final BufferedImage resultat = new BufferedImage(Fenetre.TAILLE_D_UN_CARREAU, Fenetre.TAILLE_D_UN_CARREAU, Lecteur.TYPE_DES_IMAGES);
 		final Graphics2D g2d = (Graphics2D) resultat.createGraphics();
 		int xMorceauChoisi;
 		int yMorceauChoisi;
@@ -152,23 +172,23 @@ public class Autotile {
 		if (connexionHaut) {
 			if (connexionGauche) {
 				if (connexionHautGauche) {
-					xMorceauChoisi = 2;
-					yMorceauChoisi = 4;
+					xMorceauChoisi = X_PLEIN_HAUT_GAUCHE;
+					yMorceauChoisi = Y_PLEIN_HAUT_GAUCHE;
 				} else {
-					xMorceauChoisi = 4;
-					yMorceauChoisi = 0;
+					xMorceauChoisi = X_COIN_RENTRANT_HAUT_GAUCHE;
+					yMorceauChoisi = Y_COIN_RENTRANT_HAUT_GAUCHE;
 				}
 			} else {
-				xMorceauChoisi = 0;
-				yMorceauChoisi = 4;
+				xMorceauChoisi = X_BORD_VERTICAL_HAUT_GAUCHE;
+				yMorceauChoisi = Y_BORD_VERTICAL_HAUT_GAUCHE;
 			}
 		} else {
 			if (connexionGauche) {
-				xMorceauChoisi = 2;
-				yMorceauChoisi = 2;
+				xMorceauChoisi = X_BORD_HORIZONTAL_HAUT_GAUCHE;
+				yMorceauChoisi = Y_BORD_HORIZONTAL_HAUT_GAUCHE;
 			} else {
-				xMorceauChoisi = 0;
-				yMorceauChoisi = 2;
+				xMorceauChoisi = X_COIN_SORTANT_HAUT_GAUCHE;
+				yMorceauChoisi = Y_COIN_SORTANT_HAUT_GAUCHE;
 			}
 		}
 		g2d.drawImage(this.image, 0, 0, TAILLE_MORCEAU, TAILLE_MORCEAU, 
@@ -194,11 +214,11 @@ public class Autotile {
 				xMorceauChoisi = 3;
 				yMorceauChoisi = 2;
 			} else {
-				xMorceauChoisi = 5;
-				yMorceauChoisi = 2;
+				xMorceauChoisi = X_COIN_SORTANT_HAUT_DROITE;
+				yMorceauChoisi = Y_COIN_SORTANT_HAUT_DROITE;
 			}
 		}
-		g2d.drawImage(this.image, 0, TAILLE_MORCEAU, TAILLE_MORCEAU, 2*TAILLE_MORCEAU, 
+		g2d.drawImage(this.image, TAILLE_MORCEAU, 0, 2*TAILLE_MORCEAU, TAILLE_MORCEAU, 
 				xMorceauChoisi*TAILLE_MORCEAU, yMorceauChoisi*TAILLE_MORCEAU, 
 				(xMorceauChoisi+1)*TAILLE_MORCEAU, (yMorceauChoisi+1)*TAILLE_MORCEAU, null);
 		
@@ -221,15 +241,15 @@ public class Autotile {
 				xMorceauChoisi = 2;
 				yMorceauChoisi = 7;
 			} else {
-				xMorceauChoisi = 0;
-				yMorceauChoisi = 7;
+				xMorceauChoisi = X_COIN_SORTANT_BAS_GAUCHE;
+				yMorceauChoisi = Y_COIN_SORTANT_BAS_GAUCHE;
 			}
 		}
-		g2d.drawImage(this.image, TAILLE_MORCEAU, 0, 2*TAILLE_MORCEAU, TAILLE_MORCEAU, 
+		g2d.drawImage(this.image, 0, TAILLE_MORCEAU, TAILLE_MORCEAU, 2*TAILLE_MORCEAU, 
 				xMorceauChoisi*TAILLE_MORCEAU, yMorceauChoisi*TAILLE_MORCEAU, 
 				(xMorceauChoisi+1)*TAILLE_MORCEAU, (yMorceauChoisi+1)*TAILLE_MORCEAU, null);
 		
-		//quart bas-gauche du carreau
+		//quart bas-droite du carreau
 		if (connexionBas) {
 			if (connexionDroite) {
 				if (connexionBasDroite) {
