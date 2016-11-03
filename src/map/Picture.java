3b6136abc9d837cb479f5c7aa855efc8dff1b0df
roiofthesuisse <pyer.blobfish@live.fr>
2016-11-03 18:14:53 +0100

    AfficherImage 2

--- src/map/Picture.java
+++ src/map/Picture.java
@@ -1,6 +1,10 @@
 package map;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * Image à afficher à l'écran et ses paramètres.
@@ -8,13 +12,33 @@ import java.awt.image.BufferedImage;
 public class Picture {
 	private String nomImage;
 	private BufferedImage image;
-	public int numero;
+	public Integer numero;
 	/** coordonnée x d'affichage par rapport au coin de l'écran */
 	private int x;
 	/** coordonnée y d'affichage par rapport au coin de l'écran */
 	private int y;
 	private int opacite;
 	
-	//TODO constructeur
+	/**
+	 * Constructeur explicite
+	 * @param nomImage nom du fichier image
+	 * @param numero de l'image pour le LecteurMap
+	 * @param x coordonnée x d'affichage à l'écran
+	 * @param y coordonnée y d'affichage à l'écran
+	 * @param opacite d'affichage
+	 */
+	public Picture(final String nomImage, final int numero, final int x, final int y, final int opacite) {
+		this.nomImage = nomImage;
+		try {
+			this.image = ImageIO.read(new File(".\\ressources\\Graphics\\Picture\\"+this.nomImage));
+		} catch (IOException e) {
+			System.err.println("Impossible d'ouvrir l'image "+nomImage);
+			e.printStackTrace();
+		}
+		this.numero = numero;
+		this.x = x;
+		this.y = y;
+		this.opacite = opacite;
+	}
 	
 }
