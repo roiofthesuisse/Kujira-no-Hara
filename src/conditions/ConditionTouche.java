c0c7353ab167ddb9e5f4cb6a7a0602e5f643da9c
RoiOfTheSuisse <pyer.blobfish@live.fr>
2017-11-04 14:39:17 +0100

    [ConditionTouche] Deux sortes : appui long et appui récent

--- src/conditions/ConditionTouche.java
+++ src/conditions/ConditionTouche.java
@@ -12,13 +12,18 @@ import utilitaire.GestionClavier;
 public class ConditionTouche extends Condition implements CommandeEvent {
 	
 	private final GestionClavier.ToucheRole toucheRole;
+	private final boolean toucheMaintenue;
 	
 	/** 
 	 * Constructeur explicite
-	 * @param touche à vérifier 
+	 * @param numero de la Condition
+	 * @param touche à vérifier
+	 * @param toucheMaintenue touche actuellement enfoncée VS appui récent
 	 */
-	public ConditionTouche(final GestionClavier.ToucheRole touche) {
+	public ConditionTouche(final int numero, final GestionClavier.ToucheRole touche, final boolean toucheMaintenue) {
+		this.numero = numero;
 		this.toucheRole = touche;
+		this.toucheMaintenue = toucheMaintenue;
 	}
 	
 	/**
@@ -26,19 +31,34 @@ public class ConditionTouche extends Condition implements CommandeEvent {
 	 * @param parametres liste de paramètres issus de JSON
 	 */
 	public ConditionTouche(final HashMap<String, Object> parametres) {
-		this( parametres.containsKey("touche") ? GestionClavier.ToucheRole.getToucheRole((String) parametres.get("touche")) : null);
+		this(
+				parametres.containsKey("numero") ? (int) parametres.get("numero") : -1,
+				parametres.containsKey("touche") ? GestionClavier.ToucheRole.getToucheRole((String) parametres.get("touche")) : null,
+				parametres.containsKey("toucheMaintenue") ? (boolean) parametres.get("toucheMaintenue") : true
+		);
 	}
 	
 	@Override
 	public final boolean estVerifiee() {
-		final LecteurMap lecteur = this.page.event.map.lecteur;
-		if (lecteur.frameActuelle > 1) { //pour éviter que l'Epée se déclenche en début de Map
-			final Integer frameDAppui = this.toucheRole.touche.frameDAppui;
-			if (frameDAppui != null && frameDAppui + 1 == lecteur.frameActuelle) {
-				return true;
+		if (this.toucheMaintenue) {
+			// La touche est-elle enfoncée en ce moment ?
+			if (this.toucheRole != null) {
+				return this.toucheRole.touche.enfoncee;
+			} else { 
+				return false;
 			}
+			
+		} else {
+			// Le joueur vient-il de presser la touche à l'instant ?
+			final LecteurMap lecteur = this.page.event.map.lecteur;
+			if (lecteur.frameActuelle > 1) { //pour éviter que l'Epée se déclenche en début de Map
+				final Integer frameDAppui = this.toucheRole.touche.frameDAppui;
+				if (frameDAppui != null && frameDAppui + 1 == lecteur.frameActuelle) {
+					return true;
+				}
+			}
+			return false;
 		}
-		return false;
 	}
 	
 	/**
