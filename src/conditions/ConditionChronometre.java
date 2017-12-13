b7f0677834facc454b2ea0dea23afe84427d3a2e
roiofthesuisse <pyer.blobfish@live.fr>
2017-12-13 16:25:41 +0100

    [Condition] Chronometre

---
+++ src/conditions/ConditionChronometre.java
@@ -0,0 +1,53 @@
+package conditions;
+
+import java.util.HashMap;
+
+import commandes.CommandeEvent;
+import main.Fenetre;
+import utilitaire.Maths.Inegalite;
+
+/**
+ * Est-ce que l'Event regarde dans cette Direction ?
+ */
+public class ConditionChronometre extends Condition implements CommandeEvent {
+
+	private final int nombreDeSecondes; 
+	private final Inegalite inegalite;
+	
+	/**
+	 * Constructeur explicite
+	 * @param numero de la Condition
+	 * @param nombreDeSecondes auquel on comapre le Chronometre
+	 * @param symboleInegalite = egal ; >= superieur large ; <= inferieur large ; > superieur strict ; < inferieur strict ; != différent
+	 */
+	public ConditionChronometre(final int numero, final int nombreDeSecondes, final String symboleInegalite) {
+		this.numero = numero;
+		this.inegalite = Inegalite.getInegalite(symboleInegalite);
+		this.nombreDeSecondes = nombreDeSecondes;
+	}
+	
+	/**
+	 * Constructeur générique
+	 * @param parametres liste de paramètres issus de JSON
+	 */
+	public ConditionChronometre(final HashMap<String, Object> parametres) {
+		this( parametres.containsKey("numero") ? (int) parametres.get("numero") : -1,
+			(int) parametres.get("nombreDeSecondes"),
+			(String) parametres.get("inegalite")
+		);
+	}
+	
+	@Override
+	public final boolean estVerifiee() {
+		return this.inegalite.comparer(Fenetre.getPartieActuelle().chronometre.secondes, this.nombreDeSecondes);
+	}
+	
+	/**
+	 * Ce n'est pas une Condition qui implique une proximité avec le Héros.
+	 * @return false 
+	 */
+	public final boolean estLieeAuHeros() {
+		return false;
+	}
+	
+}
