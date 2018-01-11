6795b7f05ea24db721f72e76d874467a50ba0239
RoiOfTheSuisse <pyer.blobfish@live.fr>
2018-01-11 22:16:29 +0100

    Nettoyeur de fichiers JSON

---
+++ src/main/Nettoyeur.java
@@ -0,0 +1,18 @@
+package main;
+
+/**
+ * Adapter les fichiers JSON du jeu au moteur Java sur des spécificités.
+ */
+public abstract class Nettoyeur {
+	/**
+	 * Lancer le nettoyeur.
+	 * @param args rien du tout
+	 */
+	public static void main(final String[] args) {
+		// Autotiles cousins
+		//TODO 
+		
+		// Noms des touches du clavier
+		//TODO
+	}
+}
