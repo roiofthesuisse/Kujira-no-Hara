0b398d2ac56cbe1db5b24716296e7dd573b2759d
RoiOfTheSuisse <pyer.blobfish@live.fr>
2018-01-16 22:42:19 +0100

    Nettoyeur

--- src/main/Nettoyeur.java
+++ src/main/Nettoyeur.java
@@ -9,10 +9,41 @@ public abstract class Nettoyeur {
 	 * @param args rien du tout
 	 */
 	public static void main(final String[] args) {
-		// Autotiles cousins
-		//TODO 
+		// Vérifier si le nettoyage a déjà été fait
+		if (leNettoyageADejaEteFait()) {
+			return;
+		}
 		
-		// Noms des touches du clavier
-		//TODO
+		// Autotiles cousins
+		calculerLesAutotilesCousins();
+		
+		// Noms des touches du clavier dans les messages
+		reecrireLesTouchesDuClavier();
+		
+		// Egaliser les musiques
+		egaliserLesMusiques();
+		
+	}
+
+	private static boolean leNettoyageADejaEteFait() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static void calculerLesAutotilesCousins() {
+		// TODO Auto-generated method stub
+	}
+
+	private static void reecrireLesTouchesDuClavier() {
+		// TODO Auto-generated method stub
+	}
+
+	private static void egaliserLesMusiques() {
+		//TODO calculer le volume moyen de chaque musique
+		//TODO définir 1.0 comme volume par défaut pour la plus basse
+		//TODO déduire les autres volumes par défaut par produit en croix
+		//TODO recenser les occurences de chaque musique dans le jeu avec leur volume assigné
+		//TODO pour le plus grand volumed'usage, utiliser le volume par défaut
+		//TODO déduire les remplacements des autres volumes d'usage par produit en croix 
 	}
 }
