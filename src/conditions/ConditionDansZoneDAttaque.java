243f1bc40c5ce95c406a47755d381a82df95a9eb
Roi of the Suisse <roiofthesuisse@users.noreply.github.com>
2019-05-22 13:32:24 +0200

    Update ConditionDansZoneDAttaque.java
    Etendre la notion de zone d'attaque a un Event quelconque

--- src/conditions/ConditionDansZoneDAttaque.java
+++ src/conditions/ConditionDansZoneDAttaque.java
@@ -1,43 +1,120 @@
 package conditions;
 
+import java.util.HashMap;
+
+import org.hamcrest.Condition;
+
 import commandes.CommandeEvent;
-import main.Main;
-import map.Event;
 import map.Heros;
 import map.Hitbox;
 
 /**
- * V�rifier si le H�ros est arm�, et si l'Event se trouve dans la zone d'action de son Arme
+ * Vérifier si le Héros est armé, et si l'Event se trouve dans la zone d'action de son Arme.<br>
+ * Il est possible de paramétrer cette condition pour faire attaquer egalement un event quelconque 
+ * par un autre vent quelconque avec une zone d'attaque specifiee.
  */
 public class ConditionDansZoneDAttaque extends Condition implements CommandeEvent {
-	
-	/**
-	 * Constructeur vide
-	 */
-	public ConditionDansZoneDAttaque() {
-		
-	}
-	
-	@Override
-	public final boolean estVerifiee() {
-		final boolean estCeQueLeHerosAUneArme = (getPartieActuelle().nombreDArmesPossedees > 0);
-		if (estCeQueLeHerosAUneArme) {
-			final Heros heros = this.page.event.map.heros;
-			final Event event = this.page.event;
-			final Hitbox zoneDAttaqueDuHeros = Main.getPartieActuelle().getArmeEquipee().hitbox;
-			final boolean reponse = zoneDAttaqueDuHeros.estDansZoneDAttaque(event, heros);
-			return reponse;
-		} else {
-			return false;
-		}
-	}
-	
-	/**
-	 * C'est une Condition qui implique une proximit� avec le H�ros.
-	 * @return true 
-	 */
-	public final boolean estLieeAuHeros() {
-		return true;
-	}
+
+    private final Integer idEventAttaquant;
+    private final Integer idEventCible;
+    private final String nomZone;
+
+    /**
+     * Constructeur implicite : le Heros attaque cet Event avec son Arme actuellement equipee
+     */
+    public ConditionDansZoneDAttaque() {
+        this(-1, // une condition hors code event n'a pas besoin de numero
+                0, // par defaut, Heros
+                null, // par defaut, cet Event 
+                null // par defaut, zone de l'Arme equipee du Heros
+        );
+    }
+
+    /**
+     * Constructeur explicite
+     * @param numero de la Condition
+     * @param idEventAttaquant identifiant de l'Event qui attaque
+     * @param idEventCible identifiant de l'Event qui recoit l'attaque
+     * @param idZone nom de la zone d'attaque
+     */
+    public ConditionDansZoneDAttaque(final int numero, final Integer idEventAttaquant, final Integer idEventCible,
+            final String nomZone) {
+        this.numero = numero;
+        this.idEventAttaquant = idEventAttaquant;
+        this.idEventCible = idEventCible;
+        this.nomZone = nomZone;
+}
+
+    /**
+     * Constructeur générique
+     * @param parametres liste de paramètres issus de JSON
+     */
+    public ConditionDansZoneDAttaque(final HashMap<String, Object> parametres) {
+        this(parametres.containsKey("numero") ? (int) parametres.get("numero") : -1,
+                parametres.containsKey("eventAttaquant") ? (int) parametres.get("eventAttaquant") : 0, // par defaut, Heros
+                parametres.containsKey("eventCible") ? (int) parametres.get("eventCible") : null, // par defaut, cet Event 
+                parametres.containsKey("zone") ? (String) parametres.get("zone") : null // par defaut, zone de l'Arme equipee du Heros
+        );
+    }
+
+    @Override
+    public final boolean estVerifiee() {
+        Event eventAttaquant;
+        Event eventCible;
+        Hitbox zoneDAttaque;
+        
+        // On cherche l'Event qui attaque
+        if (this.idEventAttaquant == 0) {
+            // C'est le Heros qui attaque
+            eventAttaquant = this.page.event.map.heros;
+            boolean leHerosAttaqueAvecSonArme = (this.nomZone == null);
+            if (leHerosAttaqueAvecSonArme) {
+                final boolean estCeQueLeHerosAUneArme = (getPartieActuelle().nombreDArmesPossedees > 0);
+                if (!leHerosAttaqueAvecSonArme) {
+                    // Le Heros n'a pas d'Arme !
+                    return false; 
+                }
+                zoneDAttaque = Main.getPartieActuelle().getArmeEquipee().hitbox;
+            }
+        } else if (eventAttaquant == null) {
+            // C'est cet Event qui attaque
+            eventAttaquant = this.page.event;
+        } else {
+            // C'est un Event en particulier qui attaque
+            eventAttaquant = this.page.event.map.eventsHash.get(this.idEventAttaquant);
+        }
+        
+        // On cherche l'event qui recoit l'attaque
+        if (this.idEventCible = null) {
+            // C'est cet Event qui recoit l'attaque
+            eventCible = this.page.event;
+        } else if (this.idEventCible == 0) {
+            // C'est le Heros qui attaque
+            eventCible = this.page.event.map.heros;
+        } else {
+            // C'est un Event en particulier qui recoit l'attaque
+            eventCible = this.page.event.map.eventsHash.get(this.idEventCible);
+        }
+        
+        if (zoneDAttaque == null) {
+            if (this.nomZone == null) {
+                LOG.error("Zone d'attaque non spécifiée !");
+                return false;
+            } else {
+                // TODO trouver la zone d'attaque a partir de son nom
+                // zoneDAttaque = ;
+            }
+        }
+        
+        return zoneDAttaque.estDansZoneDAttaque(eventCible, eventAttaquant);
+    }
+
+    /**
+     * C'est une Condition qui implique une proximité avec le Héros.
+     * @return true 
+     */
+    public final boolean estLieeAuHeros() {
+        return true;
+    }
 
 }
