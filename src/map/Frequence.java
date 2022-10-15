973bc66a3f065d067ea5fbee76071cf29c5cb654
roiofthesuisse <pyer.blobfish@live.fr>
2022-10-15 23:33:07 +0200

    Ajuster les frequences

--- src/map/Frequence.java
+++ src/map/Frequence.java
@@ -7,13 +7,13 @@ import org.apache.logging.log4j.Logger;
  * Fr�quence d'animation d'un Event (en frames).
  */
 public enum Frequence {
-	// Puissances de 2, avec un ratio 30/25
-	LA_PLUS_BASSE("la plus basse", 77),
-	TRES_BASSE("tres basse", 38),
-    BASSE("basse", 19),
-    HAUTE("haute", 10),
-    TRES_HAUTE("tres haute", 5),
-    LA_PLUS_HAUTE("la plus haute", 2);
+	// 1 c'est très rapide et 1000 c'est très lent
+	LA_PLUS_BASSE("la plus basse", 9),
+	TRES_BASSE("tres basse", 8),
+	BASSE("basse", 7),
+	HAUTE("haute", 6),
+	TRES_HAUTE("tres haute", 5),
+	LA_PLUS_HAUTE("la plus haute", 4);
 	
 	private static final Logger LOG = LogManager.getLogger(Frequence.class);
 	
